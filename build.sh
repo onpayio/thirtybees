24d532adbd5a0423f0050b78b9a8d70c86058101
Ebbe Sylvester Møller <esm@zitcom.dk>
2019-09-20 09:46:03 +0200

    Fix build script, checking for outdated version of composer

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-# Get composer (From composers own install instructions)
+# Get composer
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+# Run composer
 php composer-setup.php
-php -r "unlink('composer-setup.php');"
+rm composer-setup.php
 
 # Remove vendor directory
 rm -rf vendor
