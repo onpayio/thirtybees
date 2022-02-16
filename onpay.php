a76c982e189aade029c88d4fdc0285746f31f5cc
ebbesmoeller <esm@zitcom.dk>
2022-02-16 12:21:12 +0100

    Tagging new version 1.0.13

--- onpay.php
+++ onpay.php
@@ -79,7 +79,7 @@ class Onpay extends PaymentModule
     {
         $this->name = 'onpay';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.12';
+        $this->version = '1.0.13';
         $this->controllers = ['payment', 'validation'];
         $this->author = 'OnPay';
         $this->currencies = true;
